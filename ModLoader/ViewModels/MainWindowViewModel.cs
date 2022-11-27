acf315927624517bc4916f76dc3d539db620c5de
POMXARK <1997pom@gmail.com>
2022-11-27 18:59:40 +1000

    add show Image

--- ModLoader/ViewModels/MainWindowViewModel.cs
+++ ModLoader/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,31 @@
-﻿namespace ModLoader.ViewModels
+﻿using Avalonia.Media.Imaging;
+using ReactiveUI.Fody.Helpers;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ModLoader.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        [Reactive] Bitmap? Cover { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Task.Run(() => LoadCover());
+        }
+
+
+        public async Task<Stream> LoadCoverBitmapAsync()
+        {
+           return File.OpenRead("C:\\test.png");
+        }
+
+        public async Task LoadCover()
+        {
+            await using (var imageStream = await LoadCoverBitmapAsync())
+            {
+                Cover = await Task.Run(() => Bitmap.DecodeToWidth(imageStream, 400));
+            }
+        }
     }
 }

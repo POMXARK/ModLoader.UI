f0c87a68d7bfd10cddfda3b41d5628653f2ed9cf
POMXARK <1997pom@gmail.com>
2022-12-06 09:02:46 +1000

    expract mods panel

---
+++ ModLoader/Views/ModsView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ModLoader.Views
+{
+    public partial class ModsView : UserControl
+    {
+        public ModsView()
+        {
+            InitializeComponent();
+        }
+    }
+}

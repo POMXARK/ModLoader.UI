db109574db48d8fbcac76de99124a154166afea8
POMXARK <1997pom@gmail.com>
2022-12-09 10:30:59 +1000

    extract mods

--- ModLoader/ViewModels/MainWindowViewModel.cs
+++ ModLoader/ViewModels/MainWindowViewModel.cs
@@ -25,50 +25,11 @@ namespace ModLoader.ViewModels
         //	Content = new ModsViewModel(_db);	
         //}
 
-
-        private Database _db;
-
-        public ReactiveCommand<Mod, Unit> UpdateItemCommand { get; }
-
-        [Reactive] ObservableCollection<Mod> SelectedResultMods { get; set; } = new();
-        [Reactive] ObservableCollection<Mod> SelectedResultDelitedMods { get; set; } = new();
-
-        [Reactive] ObservableCollection<Pack> Packs { get; set; } = new();
-
-        [Reactive] public Pack SelectedPack { get; set; }
-
-
-        public MainWindowViewModel(Database db)
+        public MainWindowViewModel()
         {
-            _db = db;
-
-            UpdateItemCommand = ReactiveCommand.Create((Mod obj) => {  }); // UpdateItem(obj);
-
-            foreach (var item in Packs.ToList()) Packs.Remove(item);
-            foreach (var item in _db.Packs.Include(x => x.Mods).ToList()) Packs.Add(item);
-
-
-
-            this.WhenAnyValue(x => x.SelectedPack)
-                .Throttle(TimeSpan.FromSeconds(0.35), RxApp.TaskpoolScheduler)
-                .DistinctUntilChanged()
-                .Where(x => x != null)
-                .Subscribe(x =>
-                {
-                    Logger.Write(_db.ChangeTracker.DebugView.LongView);
-                    FilterPack(x.Id);
-                });
-
         }
 
-        private async void FilterPack(uint packId)
-        {
-            foreach (var item in SelectedResultMods.ToList()) SelectedResultMods.Remove(item);
-            foreach (var item in SelectedResultDelitedMods.ToList()) SelectedResultDelitedMods.Remove(item);
 
-            foreach (var item in Packs.Where(x => x.Id == packId).First().Mods.Where(x => !x.isDelited)) SelectedResultMods.Add(item);
-            foreach (var item in Packs.Where(x => x.Id == packId).First().Mods.Where(x => x.isDelited)) SelectedResultDelitedMods.Add(item);
-        }
     }
 
 
--- ModLoader/ViewModels/ModsViewModel.cs
+++ ModLoader/ViewModels/ModsViewModel.cs
@@ -1,15 +1,13 @@
 ﻿
-
-
 using Microsoft.EntityFrameworkCore;
 using ModLoader.Models;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
-using Splat.ModeDetection;
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 
@@ -28,9 +26,35 @@ namespace ModLoader.ViewModels
 
         [Reactive] ObservableCollection<Mod> DelitedMods { get; set; } = new();
 
+        public ReactiveCommand<Mod, Unit> UpdateItemCommand { get; }
+
+        [Reactive] ObservableCollection<Mod> SelectedResultMods { get; set; } = new();
+        [Reactive] ObservableCollection<Mod> SelectedResultDelitedMods { get; set; } = new();
+
+        [Reactive] ObservableCollection<Pack> Packs { get; set; } = new();
+
+        [Reactive] public Pack SelectedPack { get; set; }
+
         public ModsViewModel(Database db)
         {
             _db = db;
+
+            UpdateItemCommand = ReactiveCommand.Create((Mod obj) => { UpdateItem(obj); });
+
+            foreach (var item in Packs.ToList()) Packs.Remove(item);
+            foreach (var item in _db.Packs.Include(x => x.Mods).ToList()) Packs.Add(item);
+
+
+
+            this.WhenAnyValue(x => x.SelectedPack)
+                .Throttle(TimeSpan.FromSeconds(0.35), RxApp.TaskpoolScheduler)
+                .DistinctUntilChanged()
+                .Where(x => x != null)
+                .Subscribe(x =>
+                {
+                    Logger.Write(_db.ChangeTracker.DebugView.LongView);
+                    FilterPack(x.Id);
+                });
 
             this.WhenAnyValue(x => x.SearchText)
                 .Throttle(TimeSpan.FromSeconds(0.35), RxApp.TaskpoolScheduler)
@@ -95,7 +119,16 @@ namespace ModLoader.ViewModels
             Mods.Clear();
             DelitedMods.Clear();
             LoadMods();
-            //FilterPack(SelectedPack.Id);
+            FilterPack(SelectedPack.Id);
+        }
+
+        private async void FilterPack(uint packId)
+        {
+            foreach (var item in SelectedResultMods.ToList()) SelectedResultMods.Remove(item);
+            foreach (var item in SelectedResultDelitedMods.ToList()) SelectedResultDelitedMods.Remove(item);
+
+            foreach (var item in Packs.Where(x => x.Id == packId).First().Mods.Where(x => !x.isDelited)) SelectedResultMods.Add(item);
+            foreach (var item in Packs.Where(x => x.Id == packId).First().Mods.Where(x => x.isDelited)) SelectedResultDelitedMods.Add(item);
         }
     }
 
